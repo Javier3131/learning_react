fd98072603d0874ac894ab1641f4aedb144f6db0
Javier Lopez <javierl.1994@gmail.com>
2021-01-30 14:43:20 -0600

    Learning to use useState and reuse components.

---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  
+
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <hr></hr>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+        
+        
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
